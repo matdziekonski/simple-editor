df4b0dd5107513c7e3d73c6c6d6603d789e94587
matdziekonski <matdziekonski@gmail.com>
2019-07-10 19:00:32 +0200

    added functions to save button

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,16 @@
 import moment from 'moment';
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+ 
+
+const focusInput = document.querySelector('.form__paragraph--js');
+const save = document.querySelector('.saveButton--js');
+const load = document.querySelector('.loadButton--js');
+
+save.addEventListener('click', focusInput.value = localStorage.getItem('focuInput'));
+load.addEventListener('click', );
+
+
+focusInput.addEventListener('keyup', (e) => {
+    localStorage.setItem('focusInput', e.target.value)
+})
