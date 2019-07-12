0b310e67280cc6eb28fa8d069a3acb1f80c6114e
matdziekonski <matdziekonski@gmail.com>
2019-07-12 19:48:52 +0200

    changed buttons funcions

--- src/js/main.js
+++ src/js/main.js
@@ -11,11 +11,18 @@ const load = document.querySelector('.loadButton--js');
 
 //nie wiem czy ta funkcja powinna być przed czy po buttonach
 
-focusInput.addEventListener('keyup', (e) => {
+/*focusInput.addEventListener('keyup', (e) => {
     localStorage.setItem('focusInput', e.target.value)
 })
 
 save.addEventListener('click', focusInput.value = localStorage.setItem('focuInput'));
 load.addEventListener('click', focusInput.value = localStorage.getItem('focusInput') );
-
+*/
+save.addEventListener('click', () => {
+    localStorage.setItem('input', inputForm.value);
+  });
+  
+  load.addEventListener('click', () => {
+    inputForm.value = localStorage.getItem('input');
+  });
 
